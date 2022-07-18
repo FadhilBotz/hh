e05c63c97d45a1b174c277bffdc174ab5b1d35fc
FadhilBotz <92239117+FadhilBotz@users.noreply.github.com>
2022-07-19 03:39:30 +0700

    J

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 //=============『 Utama 』================== //
-global.owner = ['6282138919347'] 
+global.owner = ['6288708231116'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Helga Zexs'
-global.numberowner = '6282138919347'
-global.instagram = 'https://instagram.com/helga_store20'
-global.github = 'https://github.com/Hyzerr'
-global.dana = '081398860278'
+global.nameowner = 'DILL STORE'
+global.numberowner = '6288708231116'
+global.instagram = 'https://instagram.com/dill_sad17'
+global.github = 'https://github.com/FadhilBotz'
+global.dana = '088271771627'
 global.pulsa = '082138919347'
 global.gopay = '081398860278'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'ShiroBot-Md'
+global.namebot = 'Dill-Md'
 global.gc = 'https://chat.whatsapp.com/BxrDMvfLeKTHtvAWKHWZbZ'
 global.web = 'https://www.helgastoree.xyz' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
